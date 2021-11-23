7101b11a135a495eab68670c5d3481b12f420a0e
Brenna-silvaa <brennahfs12@hotmail.com>
2021-11-23 18:03:01 -0300

    criacao do layout da pagina de cadastro senha

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import LayoutPaginaSenha from './components/LayoutPaginaSenha';
+
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <LayoutPaginaSenha />
+  </div>;
 }
 
 export default App;
