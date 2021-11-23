6853ac040d3059b2bd898c63f3d852c74492ab5f
Marcelo Malenchini <malenchini4@hotmail.com>
2021-11-23 20:35:42 -0300

    feat: Criação do layout confirmação de cadastro

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import LayoutPaginaSenha from './components/LayoutPaginaSenha';
+import Cadastro from "./pages/conclusao-cadastro";
 
 function App() {
   return <div className="App">
     <LayoutPaginaSenha />
+    <Cadastro />
   </div>;
 }
 
