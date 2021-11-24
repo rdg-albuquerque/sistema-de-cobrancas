0c929c302c1d3417369b4fcb1aaa2c2cd5f0892d
RodrigoAlb <rodrigomedeiros4996@gmail.com>
2021-11-24 19:47:33 -0300

    feat: criacao e aplicação rotas protegidas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import LayoutPaginaSenha from "./pages/LayoutPaginaSenha";
 import Cadastro from "./pages/conclusao-cadastro";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthProvider";
+import { useAuth } from "./hooks/useAuth";
 
 function App() {
+  function RotasProtegidas() {
+    const { token } = useAuth();
+    return token ? <Outlet /> : <Navigate to="/login" />;
+  }
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/cadastro-2" element={<LayoutPaginaSenha />} />
-          <Route path="/cadastro-3" element={<Cadastro />} />
-        </Routes>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/cadastro-2" element={<LayoutPaginaSenha />} />
+            <Route path="/cadastro-3" element={<Cadastro />} />
+            <Route element={<RotasProtegidas />}></Route>
+          </Routes>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
