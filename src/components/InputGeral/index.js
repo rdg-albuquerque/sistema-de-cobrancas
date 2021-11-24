9fc57407c1a5d0fb2a60b861a0329775b643a94b
RodrigoAlb <rodrigomedeiros4996@gmail.com>
2021-11-23 22:06:24 -0300

    fix: inputGeral e inputSenha ragora recebem id e value em props

--- src/components/InputGeral/index.js
+++ src/components/InputGeral/index.js
@@ -1,12 +1,20 @@
 import TextField from "@material-ui/core/TextField";
 import useStyles from "./style";
 
-export default function InputGeral({ id, placeholder, type, onChange, mb }) {
+export default function InputGeral({
+  id,
+  value,
+  placeholder,
+  type,
+  onChange,
+  mb,
+}) {
   const classes = useStyles();
   return (
     <TextField
       id={id}
       className={classes.root}
+      value={value}
       placeholder={placeholder}
       size="small"
       variant="outlined"
