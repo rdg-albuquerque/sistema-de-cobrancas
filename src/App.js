1d42221b65d2a9828f2250d1fc4ecd341f329add
Marcelo Malenchini <malenchini4@hotmail.com>
2021-11-24 21:01:23 -0300

    feat: Criação de layout de login, concluído.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import LayoutPaginaSenha from './components/LayoutPaginaSenha';
+import ConfirmacaoCadastro from "./pages/conclusao-cadastro";
+import LayoutLogin from "./pages/Layout-login";
+
 
 function App() {
   return <div className="App">
     <LayoutPaginaSenha />
+    <ConfirmacaoCadastro />
+    <LayoutLogin />
   </div>;
 }
 
