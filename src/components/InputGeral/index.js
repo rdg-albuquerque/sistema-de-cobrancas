0a5a6d1698f12cf1e94069718a0b3a7def91e800
Rodrigo Albuquerque <rodrigomedeiros4996@gmail.com>
2021-11-26 19:27:35 -0300

    feat: validações do input vazio caso for obrigatório

--- src/components/InputGeral/index.js
+++ src/components/InputGeral/index.js
@@ -1,17 +1,47 @@
 import TextField from "@material-ui/core/TextField";
+import { useState } from "react";
 import useStyles from "./style";
 
-export default function InputGeral({ value, placeholder, type, onChange }) {
+export default function InputGeral({
+  value,
+  placeholder,
+  type,
+  onChange,
+  required,
+  helperText,
+}) {
   const classes = useStyles();
+  const [erro, setErro] = useState(false);
+
+  function handleChangeObrigatorio(e) {
+    setErro(false);
+    onChange(e);
+  }
+  function handleOnBlur() {
+    if (!value) {
+      setErro(true);
+    }
+  }
+
   return (
     <TextField
       className={classes.root}
       value={value}
       placeholder={placeholder}
+      InputProps={{ className: classes.input }}
       size="small"
       variant="outlined"
       type={type ? type : "text"}
-      onChange={onChange}
+      onChange={required ? handleChangeObrigatorio : onChange}
+      onBlur={required ? handleOnBlur : null}
+      error={erro}
+      helperText={
+        erro & !helperText
+          ? "Este campo é obrigatório"
+          : erro & helperText
+          ? helperText
+          : ""
+      }
     />
   );
 }
--- src/components/InputSenha/style.js
+++ src/components/InputSenha/style.js
@@ -4,12 +4,13 @@ export default makeStyles({
   root: {
     width: 368,
     "& .MuiOutlinedInput-root": {
-      borderRadius: 8,
-      "& fieldset": {},
       "&.Mui-focused fieldset": {
         borderColor: "#b20a64",
       },
     },
+  },
+  input: {
+    borderRadius: "8px",
     backgroundColor: "white",
   },
 });
