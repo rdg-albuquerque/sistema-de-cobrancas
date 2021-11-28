a9e21e8f1ed73da24822bd8ff1bd69f8bf7bc8c8
LucasRocha2308 <lucas_rocha_25@hotmail.com>
2021-11-27 21:32:44 -0300

    feat: adicionando o popup editar e sair

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,21 @@
 import "./style.css";
 import imagemPerfil from "../../assets/imagemPerfil.svg";
 import down from "../../assets/down.svg";
-// import editar from "../../assets/editar.svg";
-// import sair from "../../assets/sair.svg";
-// import { useAuth } from "../../hooks/useAuth";
+import editar from "../../assets/editar.svg";
+import sair from "../../assets/sair.svg";
+import { useAuth } from "../../hooks/useAuth";
 
 const paginaAtual = window.location.pathname;
 
-// const Popup = () => {
-//   return (
-//     <div className="popup">
-//       <img className="popup-down" src={down} />
-//       <img className="icone" src={editar} />
-//       <img className="icone" src={sair} />
-//     </div>
-//   );
-// };
+const Popup = () => {
+  return (
+    <div className="popup">
+      <img className="popup-down" src={down} />
+      <img className="icone" src={editar} />
+      <img className="icone" src={sair} />
+    </div>
+  );
+};
 
 function TituloHome() {
   if (paginaAtual === "/") {
@@ -36,11 +36,11 @@ function TituloHome() {
   //   return <h1>cobranças</h1>;
   // }
 
-  return <p></p>;
+  return <h1></h1>;
 }
 
 export function Header() {
-  // const { openPopup, setOpenPopup } = useAuth();
+  const { abrirPopup, setAbrirPopup } = useAuth();
   return (
     <div className="header">
       <TituloHome />
@@ -48,15 +48,15 @@ export function Header() {
       <div className="perfil">
         <img className="imagem-perfil" src={imagemPerfil} />
         <span className="nome-perfil"> Nome </span>
-        <img className="botao-down" src={down} />
+        {/* <img className="botao-down" src={down} /> */}
 
-        {/* <img
-          onClick={() => setOpenPopup(!openPopup ? true : false)}
+        <img
+          onClick={() => setAbrirPopup(!abrirPopup ? true : false)}
           className="botao-down"
           src={down}
-        /> */}
+        />
 
-        {/* {openPopup && <Popup />} */}
+        {abrirPopup && <Popup />}
       </div>
     </div>
   );
