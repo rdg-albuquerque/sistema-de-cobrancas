5ac3566925cc4ea921ef6b8a63b26609494c7ae5
Rodrigo Albuquerque <rodrigomedeiros4996@gmail.com>
2021-11-28 22:01:38 -0300

    fix: ajustes

--- src/components/MenuLateral/index.js
+++ src/components/MenuLateral/index.js
@@ -1,37 +1,56 @@
-
-import { useState } from "react";
-import { Link } from "react-router-dom";
 import FileCinza from "../../assets/FileCinza.svg";
 import FileRosa from "../../assets/FileRosa.svg";
-import UsersCinza from "../../assets/UsersCinza.svg";
-import UsersRosa from "../../assets/UsersRosa.svg";
 import HomeCinza from "../../assets/HomeCinza.svg";
 import HomeRosa from "../../assets/HomeRosa.svg";
-import barraRosaMenu from "../../assets/barraRosaMenu.svg";
+import UsersCinza from "../../assets/UsersCinza.svg";
+import UsersRosa from "../../assets/UsersRosa.svg";
+import "./style.css";
 
 const paginaAtual = window.location.pathname;
 
 function MenuLateral() {
-    return (
-        <div className="nav-bar">
-            <div className="itens-nav">
-                <div className={`${paginaAtual === "/" ? "novo-button" : "button-nav"}`}>
-                    <img width="29px" src={paginaAtual === "/" ? HomeRosa : HomeCinza} alt="" />
-                    <p>Home</p>
-                </div>
-
-                <div className={`${paginaAtual === "/clientes" ? "novo-button" : "button-nav"}`}>
-                    <img width="29px" src={paginaAtual === "/clientes" ? UsersRosa : UsersCinza} alt="" />
-                    <p>Clientes</p>
-                </div>
-
-                <div className={`${paginaAtual === "/cobrancas" ? "novo-button" : "button-nav"}`}>
-                    <img width="29px" src={paginaAtual === "/cobrancas" ? FileRosa : FileCinza} alt="" />
-                    <p>Cobranças</p>
-                </div>
-            </div>
+  return (
+    <div className="menu-lateral nav-bar">
+      <div className="itens-nav">
+        <div
+          className={`${paginaAtual === "/" ? "novo-button" : "button-nav"}`}
+        >
+          <img
+            width="29px"
+            src={paginaAtual === "/" ? HomeRosa : HomeCinza}
+            alt=""
+          />
+          <p>Home</p>
         </div>
-    )
+
+        <div
+          className={`${
+            paginaAtual === "/clientes" ? "novo-button" : "button-nav"
+          }`}
+        >
+          <img
+            width="29px"
+            src={paginaAtual === "/clientes" ? UsersRosa : UsersCinza}
+            alt=""
+          />
+          <p>Clientes</p>
+        </div>
+
+        <div
+          className={`${
+            paginaAtual === "/cobrancas" ? "novo-button" : "button-nav"
+          }`}
+        >
+          <img
+            width="29px"
+            src={paginaAtual === "/cobrancas" ? FileRosa : FileCinza}
+            alt=""
+          />
+          <p>Cobranças</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default MenuLateral
+export default MenuLateral;
